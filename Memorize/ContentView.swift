01d7d6fff3097fe2d1db553f821fcd5cd5d47e77
marquescami <camilamsc@hotmail.com>
2021-10-29 17:01:52 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,104 @@
+	//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Camila Marques Vasconcelos Loureiro on 26/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var emojis = ["🚗","🚕","🚌","🚎","🏎","🚓","🚑","🚛"]
+    @State var emojiCount = 4
+    
+    var body: some View {
+        VStack{
+            HStack{
+            ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                CardView(content: emoji)
+                
+            }
+        }
+        Spacer()
+        HStack{
+            remove
+            Spacer()
+            add
+        }
+        .font(.largeTitle)
+        .padding()
+    }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+}
+var remove: some View{
+    Button{
+        if emojiCount > 1 {
+            emojiCount -= 1
+        }
+    }label: {
+        Image(systemName: "minus.circle")
+    }
+}
+var add: some View {
+    Button {
+        if emojiCount < emojis.count{
+        emojiCount += 1
+    }
+} label: {
+    Image(systemName: "plus.circle")
+        }
+    }
+}
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View{
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                 shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Camila Marques Vasconcelos Loureiro on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
