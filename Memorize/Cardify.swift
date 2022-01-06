2b19579e0ecbd726c40659798acd44f832553817
marquescami <camilamsc@hotmail.com>
2022-01-06 16:56:13 -0300

    corre

--- Memorize/Cardify.swift
+++ Memorize/Cardify.swift
@@ -9,20 +9,29 @@ import SwiftUI
 
 struct Cardify: ViewModifier {
     
-    var isFaceUp: Bool
+    init (isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
+    
+    var animatableData: Double {
+        get {rotation}
+        set {rotation = newValue}
+    }
+    var rotation: Double // in degrees
     
     func body(content: Content) -> some View {
         ZStack{
             let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-            if isFaceUp {
+            if rotation < 90 {
                 shape.fill().foregroundColor(.white)
                 shape.strokeBorder(lineWidth: DrawingConstants.linewidth)
             } else {
                 shape.fill()
             }
             content
-                .opacity(isFaceUp ? 1 : 0)
+                .opacity(rotation < 90 ? 1 : 0)
         }
+        .rotation3DEffect(Angle.degrees(rotation), axis: (0,1, 0))
     }
     
     private struct DrawingConstants {
