5a3eedc80a317c6a11532d840f0322cea6e1bc92
marquescami <camilamsc@hotmail.com>
2022-01-07 21:21:10 -0300

    Ajustes nas animações dos cards e inclusão de um novo botão

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -30,7 +30,12 @@ class EmojiMemoryGame: ObservableObject{
         objectWillChange.send()
         model.choose(card)
     }
+    
     func shuffle() {
         model.shuffle()
+    }
+    
+    func restart() {
+        model = EmojiMemoryGame.createMemoryGame()
     }
 }
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -10,30 +10,85 @@ import SwiftUI
 struct EmojiMemoryGameView: View {
     @ObservedObject var game: EmojiMemoryGame
     
+    @Namespace private var dealingNamespace
+    
     var body: some View {
         VStack{
             gameBody
-            shuffle
+            deckBody
+            HStack{
+                restart
+                Spacer()
+                shuffle
+            }
+            .padding(.horizontal)
         }
         .padding()
     }
+    @State private var dealt = Set<Int>()
+    
+    private func deal(_ card: EmojiMemoryGame.Card) {
+        dealt.insert(card.id)
+    }
+    
+    private func isUndealt(_ card: EmojiMemoryGame.Card)-> Bool {
+        !dealt.contains(card.id)
+    }
+    
+    private func dealAnimation(for card: EmojiMemoryGame.Card) -> Animation {
+        var delay = 0.0
+        if let index = game.cards.firstIndex(where: { $0.id == card.id}) {
+            delay = Double(index) * (CardConstants.totalDealDuration / Double(game.cards.count))
+        }
+        return Animation.easeInOut(duration: CardConstants.dealDuration).delay(delay)
+    }
+    
+    private func zIndex(of card: EmojiMemoryGame.Card) -> Double {
+        -Double(game.cards.firstIndex(where: { $0.id == card.id }) ?? 0)
+    }
+    
     var gameBody: some View {
         AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-            if card.isMatched && !card.isFaceUp {
+            if isUndealt(card) || card.isMatched && !card.isFaceUp {
                 Color.clear
             } else {
                 CardView(card: card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
                     .padding(4)
+                    .transition(AnyTransition.asymmetric(insertion: .identity, removal: .scale))
+                    .zIndex(zIndex(of: card))
                     .onTapGesture {
                         withAnimation{
                             game.choose(card)
                         }
-
+                        
                     }
             }
         }
-        .foregroundColor(.red)
+        .foregroundColor(CardConstants.color)
     }
+    
+    var deckBody:some View {
+        ZStack{
+            ForEach(game.cards.filter(isUndealt)) { card in
+                CardView(card: card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
+                    .transition(AnyTransition.asymmetric(insertion: .opacity, removal: .identity))
+                    .zIndex(zIndex(of: card))
+            }
+        }
+        .frame(width: CardConstants.undealwidth, height: CardConstants.undealHeight)
+        .foregroundColor(CardConstants.color)
+        .onTapGesture {
+            // "distribuir" cartas na IU
+            for card in game.cards {
+                withAnimation(dealAnimation(for: card)){
+                    deal(card)
+                }
+            }
+        }
+    }
+    
     var shuffle: some View{
         Button("Shuffle"){
             withAnimation(){
@@ -41,8 +96,25 @@ struct EmojiMemoryGameView: View {
             }
         }
     }
+    
+    var restart: some View {
+        Button("Restart"){
+            withAnimation{
+                dealt = []
+                game.restart()
+            }
+        }
+    }
+    
+    private struct CardConstants {
+        static let color = Color.red
+        static let aspectRatio: CGFloat = 2/3
+        static let dealDuration: Double = 0.5
+        static let totalDealDuration: Double = 2
+        static let undealHeight: CGFloat = 90
+        static let undealwidth = undealHeight * aspectRatio
+    }
 }
-
 
 struct CardView: View {
     let card: EmojiMemoryGame.Card
