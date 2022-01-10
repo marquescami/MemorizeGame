1199c1d45e82cc86268dee1aca5a302465091dbb
marquescami <camilamsc@hotmail.com>
2022-01-10 14:47:33 -0300

    Finalização memorize aulas de Stanford

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -126,12 +126,24 @@ struct CardView: View {
     var body: some View{
         GeometryReader { geometry in
             ZStack{
-                Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 0-90))
+                Group{
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: (1-animatedBonusRemaining)*360-90))
+                            .onAppear {
+                                withAnimation(.linear(duration: card.bonusRemaining)){
+                                    animatedBonusRemaining = 0
+                                }
+                            }
+                    } else {
+                        Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: (1-card.bonusRemaining)*360-90))
+                    }
+                }
                     .padding(5)
                     .opacity(0.5)
                 Text(card.content)
                     .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
                     .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                    .padding(5)
                     .font(Font.system(size: DrawingConstants.fontSize))
                     .scaleEffect(scale(thatFits: geometry.size))
             }
