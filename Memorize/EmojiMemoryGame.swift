977006f3caa615ceb3de7d923f38cf63a0910811
marquescami <camilamsc@hotmail.com>
2022-01-10 15:05:16 -0300

    Alteração dos emojis e quantidade de cartas

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -10,10 +10,10 @@ import SwiftUI
 class EmojiMemoryGame: ObservableObject{
     typealias Card = MemoryGame<String>.Card
     
-    private static let emojis = ["🚗","🚌","🏎","🚓","🚑","🚎","🚛","🚡","🛻","🚒", "🚗","🚌","🏎","🚓","🚑","🚎","🚛","🚡","🛻","🚒"]
+    private static let emojis = ["🗽","✈️","🏝","🛬","🚤","🗿","🏖","🚢"]
     
     private static func createMemoryGame() -> MemoryGame<String> {
-        MemoryGame<String>(numberOfPairsOfCards: 6) { pairIndex in
+        MemoryGame<String>(numberOfPairsOfCards: 8) { pairIndex in
             emojis[pairIndex]
         }
     }
